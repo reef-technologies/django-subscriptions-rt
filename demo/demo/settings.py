cc25ea16474fb01a557e56d76e3d76b351768754
Krzysztof Kalinowski <krzysztof.kalinowski@reef.pl>
2022-10-21 07:22:25 +0200

    Ability to create users on a whim using validation function

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -152,3 +152,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Function that accepts a request and returns ID of the user. Returns None if the user is not authenticated.
+HEADER_VALIDATION_FUNCTION = None
--- subscriptions/middleware.py
+++ subscriptions/middleware.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
 from django.utils.deprecation import MiddlewareMixin
 
 from .functions import get_remaining_amount
@@ -6,6 +9,14 @@ from .models import Subscription
 
 class SubscriptionsMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        is_auth = request.user.is_authenticated
+        if settings.HEADER_VALIDATION_FUNCTION is None:
+            is_auth = request.user.is_authenticated
+        else:
+            # There, most probably, is no user. If that's the case, we need to create him.
+            user_id = settings.HEADER_VALIDATION_FUNCTION(request.headers)
+            is_auth = user_id is not None
+            if is_auth:
+                request.user = User.objects.get_or_create(username=user_id, password=make_password(None))
+
         request.user.active_subscriptions = Subscription.objects.filter(user=request.user).active() if is_auth else []
         request.user.quotas = get_remaining_amount(user=request.user) if is_auth else {}
