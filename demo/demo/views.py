d32bee60f4fb4a6f4592df5dba63da19d8a03e2b
Aleksandr Goncharov <aleksandr.goncharov@reef.pl>
2023-05-27 20:06:16 +0300

    Add missing views.py file to demo app

---
+++ demo/demo/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+
+from subscriptions.api.views import ResourceHeadersMixin
+
+
+class ResourceHeadersMixinTestView(ResourceHeadersMixin):
+
+    def get(self, request):
+        return HttpResponse('ok')
